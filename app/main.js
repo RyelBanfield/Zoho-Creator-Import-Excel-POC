9657dbbd12622e392208f76a4979b5724d2738e6
Ryel Banfield <24265119+RyelBanfield@users.noreply.github.com>
2021-10-10 07:39:30 -0400

    Adjust main import feature

--- app/main.js
+++ app/main.js
@@ -1,23 +1,12 @@
-import { unflatten } from './modules/flatten.js';
+import { flatten, unflatten } from './modules/flatten.js';
 
-let sheetType = null;
 let submittedFile = null;
 
-const sheetTypeDropdown = document.getElementById('sheet-type');
 const chooseFileBtn = document.getElementById('chooseFile');
 const extractDataBtn = document.getElementById('extractData');
-const getRecordsBtn = document.getElementById('getRecords');
-const exportExcelBtn = document.getElementById('exportExcel');
+// const getRecordsBtn = document.getElementById('getRecords');
+// const exportExcelBtn = document.getElementById('exportExcel');
 
-// Set the type of excel sheet being used
-sheetTypeDropdown.addEventListener('change', (event) => {
-  sheetType = event.target.value;
-  sheetType === ''
-    ? (chooseFileBtn.disabled = true)
-    : (chooseFileBtn.disabled = false);
-});
-
-// Upload file and hold on to it
 chooseFileBtn.addEventListener('change', (event) => {
   submittedFile = event.target.files[0];
   submittedFile === null
@@ -25,7 +14,6 @@ chooseFileBtn.addEventListener('change', (event) => {
     : (extractDataBtn.disabled = false);
 });
 
-// Extract data from uploaded excel sheet
 extractDataBtn.addEventListener('click', () => {
   if (submittedFile) {
     const excelData = {};
@@ -36,59 +24,37 @@ extractDataBtn.addEventListener('click', () => {
         type: 'binary',
       });
 
-      // Create an array of objects for each sheet and save it to the excelData object
       workbook.SheetNames.forEach((sheet) => {
         const row = XLSX.utils.sheet_to_row_object_array(
           workbook.Sheets[sheet]
         );
-        // Save the name of the sheet as the key and the array of objects as the value
         excelData[`${sheet}`] = row;
       });
-      console.log(unflatten(excelData.Customer[0]));
 
-      // ZOHO.CREATOR.init().then(function (data) {
-      // for (sheet in excelData) {
-      //   for (row in excelData[sheet]) {
-      //     let formData = {};
+      ZOHO.CREATOR.init().then(() => {
+        for (let sheet in excelData) {
+          for (let row in excelData[sheet]) {
+            let formData = {
+              data: unflatten(excelData[sheet][row]),
+            };
 
-      //     switch (sheetType) {
-      //       case 'Customers-Salespersons':
-      //         formData = {
-      //           data: {
-      //             [`${sheet}_Name`]: {
-      //               display_value: `${excelData[sheet][row].First_Name} ${excelData[sheet][row].Last_Name}`,
-      //               first_name: `${excelData[sheet][row].First_Name}`,
-      //               last_name: `${excelData[sheet][row].Last_Name}`,
-      //             },
-      //             Email: excelData[sheet][row].Email,
-      //             Phone_Number: excelData[sheet][row].Phone_Number,
-      //           },
-      //         };
-      //         break;
-      //       case 'Sales':
-      //         formData = {
-      //           data: {},
-      //         };
-      //         break;
-      //     }
+            console.log(sheet, formData.data);
 
-      //     console.log(sheet, formData.data);
+            const config = {
+              formName: sheet,
+              data: formData,
+            };
 
-      //     // const config = {
-      //     //   formName: sheet,
-      //     //   data: formData,
-      //     // };
-
-      //     // ZOHO.CREATOR.API.addRecord(config).then(function (response) {
-      //     //   if (response.code == 3000) {
-      //     //     console.log('Record added successfully');
-      //     //   } else {
-      //     //     console.log(response);
-      //     //   }
-      //     // });
-      //   }
-      // }
-      // });
+            ZOHO.CREATOR.API.addRecord(config).then((response) => {
+              if (response.code == 3000) {
+                console.log('Record added successfully');
+              } else {
+                console.log(response);
+              }
+            });
+          }
+        }
+      });
     };
     fileReader.readAsBinaryString(submittedFile);
   }
